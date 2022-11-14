973e9fdc41d7362b68cc2ee72017fe7386faa0f9
Hans Golong <hans.golong@gmail.com>
2022-11-14 14:03:27 +0800

    VRNN Training code

--- v_rcnn/dataset.py
+++ v_rcnn/dataset.py
@@ -1,19 +1,28 @@
-import glob
 import time
 from typing import Dict, List, Tuple
-from utils import get_iou_score, read_image_cv2, get_ss_boxes, save_img, plot_image_with_bb
+from utils import get_iou_score, read_image_cv2, get_ss_boxes, plot_image_with_bb
 import pandas as pd
 from ast import literal_eval
 import os
-import shutil
 from torch_snippets import *
 from sklearn.model_selection import train_test_split
 from data_augment import class_to_id
 import pickle
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 IOU_POSITIVE_MATCH = 0.3
 MAX_RECT = 2000
+FE_INPUT_W = 224
+FE_INPUT_H = 224
+
 _SAMPLE_SEED = 42
+
+# For use in normalization
+# Uses ImageNet values for normalization since we are using a pretrained ResNet50 network
+# Addtnl Ref: https://stackoverflow.com/questions/58151507/why-pytorch-officially-use-mean-0-485-0-456-0-406-and-std-0-229-0-224-0-2
+imagenet_normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
 
 class RCNNDataset(Dataset):
 
@@ -170,14 +179,66 @@ class RCNNDataset(Dataset):
         with open(save_fp, 'wb') as fs:
             pickle.dump(data, fs)
 
-    def __getitem__(self, ix):
-        pass
+    def __getitem__(self, img_idx):
+        
+        # Get the image at this idx
+        img_filepath = os.path.join(self.img_base_dir, self.filenames[img_idx])
+        img = read_image_cv2(img_filepath)
+        H, W, _ = img.shape
+
+        # Get the region proposals for this image
+        rois = (self.rois[img_idx] * np.array([W,H,W,H])).astype(np.uint16)
+        roi_crops = [img[y:Y,x:X] for (x,y,X,Y) in rois]
+
+        # Get the roi classes and roi offsets
+        roi_classes = self.roi_classes[img_idx]
+        roi_deltas = self.roi_deltas[img_idx]
+
+        return roi_crops, roi_classes, roi_deltas
 
     def collate_fn(self, batch):
-        pass
+
+        roi_crops = [] 
+        roi_classes = []
+        roi_deltas = []
+
+        # Process batch
+        for ix in range(len(batch)):
+            _roi_crops, _roi_classes, _roi_deltas = batch[ix]
+
+            # Resize to input size of 224x224
+            _roi_crops = [cv2.resize(crop, (FE_INPUT_W, FE_INPUT_H)) for crop in _roi_crops]
+            for _roi_crop in _roi_crops:
+                # Turn to (C,H,W) from (H,W,C)
+                _roi_crop = torch.tensor(_roi_crop).permute(2,0,1)
+                # Turn pixel values as a % of 255's
+                _roi_crop = _roi_crop/255.
+                # Normalize using normalization values of ImageNet
+                _roi_crop = imagenet_normalize(_roi_crop)
+                # Make sure we are dealing with a float tensor
+                _roi_crop = _roi_crop.to(device).float()
+                # Expand first dim so torch.cat works later
+                _roi_crop = torch.unsqueeze(_roi_crop, 0)
+                # Append to list
+                roi_crops.append(_roi_crop)
+            
+            # Just extend the lists with the rois
+            roi_classes.extend(_roi_classes)
+            roi_deltas.extend(_roi_deltas)
+        
+        # Check. Expect all lists to have the len of rois
+        assert (len(roi_crops) == len(roi_classes) 
+                and len(roi_classes) == len(roi_deltas))
+
+        # Create batched tensors
+        roi_crops = torch.cat(roi_crops).to(device)
+        roi_classes = torch.Tensor(roi_classes).long().to(device)
+        roi_deltas = torch.Tensor(roi_deltas).float().to(device)
+
+        return roi_crops, roi_classes, roi_deltas
     
     def __len__(self): 
-        return len(self.data_df)
+        return len(self.filenames)
 
 def create_train_test_dataset(img_root_dir:str, pd_csv_path:str, limit:int=None) -> Tuple[Dataset, Dataset]:
     """Creates the train and test datasets for V-RCNN training and eval
--- v_rcnn/model.py
+++ v_rcnn/model.py
@@ -1,24 +1,91 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
-from data_augment import id_to_class
+from data_augment import class_to_id, id_to_class
+from torchvision import models
 
+background_class = class_to_id['background']
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class Net(nn.Module):
+class RCNN(nn.Module):
+
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(44944, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, len(id_to_class))
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        # Use a resnet backbone
+        resnet_backbone = models.resnet50(weights = models.ResNet50_Weights.IMAGENET1K_V2)
+        # Output shape will be (1024, 14, 14)
+        model = torch.nn.Sequential(*(list(resnet_backbone.children())[:-3]))
+        for param in model.parameters():
+            param.requires_grad = False
+        model.eval().to(device)
+        self.backbone = model
+        
+        # Flattened shape of output from resnet50
+        feature_dim = 1024 * 14 * 14
+        # 2 FC layers
+        self.fc1 = nn.Linear(feature_dim, 4096)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.relu = nn.ReLU()
+
+        # Classifier to label
+        self.cls_score = nn.Sequential(
+            nn.Linear(4096, len(id_to_class)),
+            nn.Softmax(-1)
+        )
+
+        # BBox offset linear regressor
+        self.bbox = nn.Sequential(
+              nn.Linear(4096, 512),
+              nn.ReLU(),
+              nn.Linear(512, 4),
+              nn.Tanh(),
+            )
+
+        self.bb_loss_weight = 10
+        self.cel = nn.CrossEntropyLoss()
+        self.sl1 = nn.L1Loss()
+
+    def forward(self, input):
+
+        # Extract features using resnet backbone
+        fmap = self.backbone(input)
+        
+        # Pass features through FC layers
+        fmap_flat = torch.flatten(fmap, start_dim = 1)
+        fc1_op = self.fc1(fmap_flat) 
+        fc1_op = self.relu(fc1_op)
+        fc2_op = self.fc2(fc1_op)
+        fc2_op = self.relu(fc2_op)
+        
+        # Get estimated classification probabilities
+        cls_score = self.cls_score(fc2_op)
+        # Get estimated bbox offset
+        bbox = self.bbox(fc2_op)
+
+        return cls_score, bbox
+
+    def calc_loss(self, pred_classes, pred_bb_offset, gt_classes, gt_bb_offset):
+
+        # Get classification loss
+        classification_loss = self.cel(pred_classes, gt_classes)
+
+        # Keep only bb's that aren't background
+        bb_idxs = torch.where(gt_classes != background_class)[0]
+        # If have bb's that aren't background, calculate bb offset loss
+        if len(bb_idxs) > 0:
+            pred_bb_offset = pred_bb_offset[bb_idxs]
+            gt_bb_offset = gt_bb_offset[bb_idxs]
+            regression_loss = self.sl1(pred_bb_offset, gt_bb_offset)
+        # Else no bb offset regression loss for all-backgound proposals
+        else:
+            regression_loss = torch.zeros(1, device=device)
+
+        # Calculate all_loss
+        all_loss = classification_loss + self.bb_loss_weight * regression_loss
+
+        # Detach non-all loss losses
+        classification_loss = classification_loss.detach()
+        regression_loss = regression_loss.detach()
+
+        return all_loss, classification_loss, regression_loss
